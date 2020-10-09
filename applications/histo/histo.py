0706b904ddae96789438200264b4df797bfa675c
Mosharraf Musa <mosharrafny@gmail.com>
2020-10-08 21:37:11 -0400

    sum_diff completed

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,1 +1,8 @@
 # Your code here
+esc = {char for char in ['\t', '\n', '\r']}
+
+
+def word_count(s):
+
+    # Your code here
+    pass
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,20 +1,33 @@
-cache = {}
+import sys
+import re
 
-chars_to_ignore = " \" : ; , . - + = / \ | [ ] { } ( ) * ^ &"
+# sys.path.insert(1, '/home/ubuntom/Projects/lambda/cs-module-project-hash-tables')
+
+# from hashtable.hashtable import HashTable
+esc = {char for char in ['\t', '\n', '\r']}
 
 
 def word_count(s):
     # Your code here
-    # returns a words with their counts
-    words = s.lower().split()
-    # print(words)
+    ht = {}
+    word = ""
+    special = {char for char in '":;,.-+=/\\|[]{}()*^& '}
+    s = s.replace('\r', ' ').replace('\n', ' ').replace('\t', ' ')
 
-    # strips the chars out while making a new array
-    new_words = [word.strip(chars_to_ignore) for word in words]
-
-    for word in new_words:
-        cache[word] = new_words.count(word)
-    return cache
+    def add_word(word):
+        if word not in ht:
+            ht[word] = 1
+        else:
+            ht[word] += 1
+    for i in range(len(s)):
+        if s[i] not in special:
+            word += s[i].lower()
+        if s[i] == ' ' and word:
+            add_word(word)
+            word = ''
+    if word:
+        add_word(word)
+    return ht
 
 
 if __name__ == "__main__":
@@ -23,3 +36,6 @@ if __name__ == "__main__":
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count(
         'This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count("Hello    hello"))
+    print(word_count('":;,.-+=/\\|[]{}()*^&'))
+    print(word_count('a a\ra\na\ta \t\r\n'))
